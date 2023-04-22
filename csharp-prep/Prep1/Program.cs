a268aac7fb4a09dd802f34b76250cb366657e712
artemisiaporto <artemisiaporto@outlook.com>
2023-04-22 20:31:35 -0300

    Finished Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Ask the user for their first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        //Ask the user for their last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        //Shows the message using the user's first and last name
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
