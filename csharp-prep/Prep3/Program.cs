cf60bac7acc68fcae7c96260deaf5f4c85854043
artemisiaporto <artemisiaporto@outlook.com>
2023-04-27 00:22:57 -0300

    Finished Prep3 core requirement 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magical number? ");
+        string answer1 = Console.ReadLine();
+        int magicalNumber;
+        magicalNumber = int.Parse(answer1);
+
+        Console.Write("What is your guess? ");
+        string answer2 = Console.ReadLine();
+        int guess = int.Parse(answer2);
+
+        if (magicalNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        if (magicalNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        if (magicalNumber == guess)
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
