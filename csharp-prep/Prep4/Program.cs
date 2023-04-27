bddf731b7f1b0999abf3cad60c9ba3cdf8c3b730
artemisiaporto <artemisiaporto@outlook.com>
2023-04-27 19:19:56 -0300

    Finished Prep4 core requirement 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number;
+        int sum;
+        List<int> numbers = new List<int>();
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string answer = Console.ReadLine();
+            number = int.Parse(answer);
+            numbers.Add(number);
+        } while (number != 0);
+
+        sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
     }
 }
